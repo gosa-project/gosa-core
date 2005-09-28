9a1d0c542de4e235083a3cc791df4134e5e6f43c
janw <janw@594d385d-05f5-0310-b6e9-bd551577e9d8>
2005-09-28 20:02:55 +0000

    Updated Documentation.
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@1440 594d385d-05f5-0310-b6e9-bd551577e9d8

--- gen_function_list.php
+++ gen_function_list.php
@@ -3,7 +3,7 @@
 /*
 /*
  * This code is part of GOsa (https://gosa.gonicus.de)
- * Copyright (C) 2003 Cajus Pollmeier
+ * Copyright (C) 2005 Jan Wenzel
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
