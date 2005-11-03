447b0b1d8b38dc528ac09bb3f99b2f62ebe49263
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2005-11-03 09:49:27 +0000

    Changed help functions
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@1810 594d385d-05f5-0310-b6e9-bd551577e9d8

--- html/helpviewer.php
+++ html/helpviewer.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 /*
    This code is part of GOsa (https://gosa.gonicus.de)
    Copyright (C) 2003  Cajus Pollmeier, Fabian Hickert
@@ -18,18 +19,20 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
-/* Basic setup, remove eventually registered sessions */
+/* Include classes and configs */
 require_once ("../include/php_setup.inc");
 require_once ("functions.inc");
 require_once ("functions_helpviewer.inc");
+
 header("Content-type: text/html; charset=UTF-8");
+session_start();
 
-/* Start session */
-session_start ();
-
-/* Display all Errors/Warnings*/
-error_reporting(E_ALL);
+/* If no config object is found in the session, abort help */
+if (!isset($_SESSION['config'])){
+  gosa_log ("main.php called without session");
+  header ("Location: index.php");
+  exit;
+}
 
 /* Language setup */
 if ($config->data['MAIN']['LANG'] == ""){
@@ -45,21 +48,11 @@ setlocale(LC_ALL, $lang);
 $GLOBALS['t_language']= $lang;
 $GLOBALS['t_gettext_message_dir'] = $BASE_DIR.'/locale/';
 
-/* Set the text domain as 'messages' */
 $domain = 'messages';
 bindtextdomain($domain, "$BASE_DIR/locale");
 textdomain($domain);
 @DEBUG (DEBUG_TRACE, __LINE__, __FUNCTION__, __FILE__, $lang, "Setting language to");
 
-
-/* Logged in? Simple security check */
-if (!isset($_SESSION['ui'])){
-  gosa_log ("Error: helpviewer.php called without session");
-  echo "<b>"._("Help is not available if you are not logged in.")."</b>";
-  exit;
-}
-
-/* Set template compile directory */
 $config= $_SESSION['config'];
 if (isset ($config->data['MAIN']['COMPILE'])){
   $smarty->compile_dir= $config->data['MAIN']['COMPILE'];
@@ -67,110 +60,169 @@ if (isset ($config->data['MAIN']['COMPILE'])){
   $smarty->compile_dir= '/var/spool/gosa/';
 }
 
-/* Get plugin list */
-$plist= $_SESSION['plist'];
 
-/*
-##################
-My PART ^^
-##################
+
+/* HELP management starts here ...
  */
 
-$helpdir                      = "/home/hickert/gosa/doc/guide/user/fr/html/"; // Folder to use for help files
+
+/* Generate helpobject */
+if(isset($_SESSION['helpobject'])){
+  $helpobject = $_SESSION['helpobject'];
+}else{
+  $helpobject['lang']         = "en"; 
+  $helpobject['helpconf']     = array();  
+  $helpobject['currentplug']  = "";
+  $helpobject['file']         = "index.html";
+  $helpobject['helpconf']     = $_SESSION['plist']->gen_headlines();
+}
+
+$lang =  preg_replace("/_.*$/","",get_browser_language());
+
 $defaultpage                  = "index.html";                       // alternative file, shown on error, or on first call
 $prefix                       = "node";                             // Prefix of the generated help files 
 $suffix                       = ".html";                            // Suffix of the generated helpfiles
 $maxresults                   = 10;                                 // max number of results shown in result list
 $minwordlength                = 3;                                  // Word less than 3 chars will be dropped in search
 $allowed_chars_in_searchword  = "'[^a-z0-9 %_-]'i";                 // Remove all chars that would disturb our search like < or > ...
-$pre_mark                     = "<span style=\"background-color: #FFFC35;\">" ;                       // Sign words with this
-$suf_mark                     = "</span>";                          //  and this
-$backwardlink                 = "<a href=\"?pg=%s\"  class=\"maintitlebar\">
-                                  <img src='images/back.png' align=\"middle\" alt=\""._("previous")."\" border=\"0\">
-                                 </a>";
-
-$forwardlink                 = "<a href=\"?pg=%s\"  class=\"maintitlebar\">
-                                  <img src='images/forward.png' align=\"middle\" alt=\""._("next")."\" border=\"0\">
-                                 </a>";
-
 
 /* Default pages */
 $backward =$defaultpage;
 $index    =$defaultpage;
 $forward  ="node1.html";
 
-/*
-   Here it begins, the real function, above only definitions
+
+/* Every class which is called within a tab, stores its name in the Session.
+ * If $_SESSION['current_class_for_help'] isset, 
+ *  get the helpfile specified in the xml file and display it.
+ * Unset this Session entry, to avoid displaying it again.
  */
-/* Path exists ? */
-if((is_dir($helpdir))&&($fp = opendir($helpdir))){
+if(isset($_SESSION['current_class_for_help'])){
 
-  /* read all available directories */
-  while($dir = readdir($fp)){
+  /* Create new XML parser with the path to the Xml file */
+  $xml = new parseXml("../doc/guide.xml");
+  
+  /* Generate help array */
+  $str = $xml->parse();
+  
+  /* __LANG__ is used as placeholder for the used language*/
+  $helpdir= preg_replace("/__LANG__/i",$lang,$str[($_SESSION['current_class_for_help'])]['PATH']);
+
+  /* If there is no entry in the xml file for this class, display an error message */
+  if($helpdir == ""){
     
-    /* Skip . / .. */
-    if(!in_array($dir,array(".","..","images"))){
-      $arr[$dir] = readfiles($helpdir."/".$dir."/",$prefix,$suffix,false,$singlepage=false);
-    }
+    $smarty->assign("help_contents","<br><h2>"._("There is no helpfile specified for this class."))."</h2>";
+    $header= "<!-- headers.tpl-->".$smarty->fetch(get_template_path('headers.tpl'));
+    $display= utf8_encode(  $header.$smarty->fetch(get_template_path('help.tpl')));
+    echo $display;
+    unset($_SESSION['current_class_for_help']);
+    exit();  
   }
-    
-  /* Handle posts */
-  if((isset($_GET['folder']))&&(is_dir($helpdir."/".$_GET['folder']."/"))){
-    $_SESSION['helpbrowser']['folder'] = $_GET['folder'];
-  }
+  
+  /* Save filename */
+  $helpobject['file']= $str[($_SESSION['current_class_for_help'])]['FILE'];
+  
+  /* Save path to the file */
+  $helpobject['currentplug']  = $helpdir;
+  
+  /* Avoid displaying the same help every time */
+  unset($_SESSION['current_class_for_help']);
 
-  if((isset($_GET['folder']))&&(empty($_GET['folder']))){
-    $_SESSION['helpbrowser']['folder']  = "";
-    $_SESSION['helpbrowser']['file']    = "";
+}elseif(isset($_GET['plug'])){
+  /* This displays helpfiles depending on the current $_GET[plug] */
+  $tmp                          = new pluglist($_SESSION['config'],NULL);
+  $path                         = $tmp->get_path($_GET['plug']);
+  $helpobject['currentplug']    = $path;
+  $helpobject['file']           = "index.html";
+  $helpdir = "../doc/guide/user/".$helpobject['lang']."/html/".preg_replace("/^.*\//i","",$helpobject['currentplug']);
+  if(empty($helpobject['currentplug'])){
+    $helpdir= "";
   }
-
-  if((isset($_GET['pg']))&&(empty($_GET['pg']))){
-    $_SESSION['helpbrowser']['folder']  = "";
-    $_SESSION['helpbrowser']['file']    = "";
-  }
-
-  if((isset($_GET['pg']))&&(is_file($helpdir."/".$_SESSION['helpbrowser']['folder']."/".$_GET['pg']))){
-    $_SESSION['helpbrowser']['file'] = $_GET['pg'];
-  }
-
-  /* Open helppage */
-  if(!empty($_SESSION['helpbrowser']['folder'])){
-    $folder = $_SESSION['helpbrowser']['folder'];
-    if(!empty($_SESSION['helpbrowser']['file'])){
-      $file = $_SESSION['helpbrowser']['file'];
-      $index = readfiles($helpdir."/".$folder."/",$prefix,"",false,$file);
-      $smarty->assign("help_contents",$index[$file]['content']);
-    }else{
-      $index = readfiles($helpdir."/".$folder."/",$prefix,"",false,$singlepage="index.html");
-      $smarty->assign("help_contents",$index['index.html']['content']);
-    }
+}
+/* this Post var is set if another page is requested */
+if(isset($_GET['pg'])){
+ 
+  if(preg_match("/\//",$_GET['pg'])){
+    $arr = split("\/",$_GET['pg']);
+    $helpobject['currentplug'] = "../doc/guide/user/".$helpobject['lang']."/html/".$arr[0];
+    $helpdir = $helpobject['currentplug'];
+    $helpobject['file']= $arr[1];
   }else{
-    $smarty->assign("help_contents",genIndex($arr));
+    /* PG should contain a filename */
+    $helpobject['file'] = $_GET['pg'];
+
+
+    /* If empty, force displaying the index */
+    if(empty($_GET['pg'])){
+      $helpobject['currentplug']  = "";
+      $helpobject['file'] = "index.html";
+    }
+    /* Create new helpdir (The path where the requested page is located)*/
+    $helpdir = "../doc/guide/user/".$helpobject['lang']."/html/".preg_replace("/^.*\//i","",$helpobject['currentplug']);
+
+    /* If helpdir is empty, force index */
+    if(empty($helpobject['currentplug'])){
+      $helpdir= "";
+    }
   }
+}
+
+$helpdir.="/";
+
+/* Save current settings */
+$_SESSION['helpobject'] = $helpobject;
+
+/* 
+ * Display management 
+ */
+
+/* If there is no helpdir or file defined, display the index */
+if(isset($_POST['search'])){
+  $helpdir = "../doc/guide/user/".$helpobject['lang']."/html/";
+  /* read all available directories */
+  $index = readfiles($helpdir,$prefix,$suffix,false,false);
+  $smarty->assign("help_contents",((searchlist($index,search($index,$_POST['search_string']),10))));
   $header= "<!-- headers.tpl-->".$smarty->fetch(get_template_path('headers.tpl'));
+
+  /* I don't know why, but we must use utf8_encode to avoid dispplay errors */
   $display= utf8_encode(  $header.$smarty->fetch(get_template_path('help.tpl')));
   echo $display;
+}elseif(((empty($helpdir)))||($helpdir=="/")){
+  /* Generate Index and display it */
+  $smarty->assign("help_contents",genIndex());
+  $header= "<!-- headers.tpl-->".$smarty->fetch(get_template_path('headers.tpl'));
+
+  /* I don't know why, but we must use utf8_encode to avoid dispplay errors */
+  $display= utf8_encode(  $header.$smarty->fetch(get_template_path('help.tpl')));
+  echo $display;
+
+}elseif((is_dir($helpdir))&&($fp = opendir($helpdir))){
+  
+  /* Readfile gets the content of the requested file, 
+   * parse it, rework links images and so on */
+  $index = readfiles($helpdir,$prefix,$suffix,false,$helpobject['file']);
+
+  /* if this page is result from a search, mark the search strings */
+  if(isset($_GET['mark'])){
+    $matches = $_SESSION['lastresults'][preg_replace("/^.*\//i","",$helpobject['currentplug'])][$helpobject['file']];
+    $index[$helpobject['file']]['content']   = markup_page($index[$helpobject['file']]['content'],$matches);
+  }
+
+  /* Display the help contents */
+  $smarty->assign("help_contents",$index[$helpobject['file']]['content']);
+  $header= "<!-- headers.tpl-->".$smarty->fetch(get_template_path('headers.tpl'));
+  
+  /* I don't know why, but we must use utf8_encode to avoid dispplay errors */
+  $display= utf8_encode(  $header.$smarty->fetch(get_template_path('help.tpl')));
+  echo $display;
+
 }else{
+  
+  /* There was a file requested which actually doesn't exists */
   $smarty->assign("help_contents","<h2>".sprintf(_("Helpdir '%s' is not accessible, can't read any helpfiles."),$helpdir))."</h2><br>";
   $header= "<!-- headers.tpl-->".$smarty->fetch(get_template_path('headers.tpl'));
   $display= utf8_encode(  $header.$smarty->fetch(get_template_path('help.tpl')));
   echo $display;
 }
-
-function genIndex($arr)
-{
-  $str = "<h2>"._("Index")."</h2>";
-  foreach($arr as $index => $obj){    
-    $nr   = preg_replace("/_.*$/i","",$index);
-    $name = preg_replace("/^.*_/i","",$index);
-    if(empty($name)){
-      $name = preg_replace("/_/","",$index);
-      $nr = "";
-    }
-    $str .= "<p style='padding-left:20px;'><a href='?folder=".$index."'><b>".$nr." - ".$name."</b></a></p>";
-  }
-  return $str;
-}
-
 // vim:tabstop=2:expandtab:shiftwidth=2:filetype=php:syntax:ruler:
 ?>
