e66c241db9a1f0349b0f02b75cfb86f72223f8cc
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2006-02-23 14:01:30 +0000

    Added script which allow downloading of FAIscripts- use http://gosa/getFAIscript.php?id=ASDFdfaddfs== << base64_encoded dn
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@2732 594d385d-05f5-0310-b6e9-bd551577e9d8

---
+++ html/getFAIscript.php
@@ -0,0 +1,73 @@
+<?php
+/*
+   This code is part of GOsa (https://gosa.gonicus.de)
+   Copyright (C) 2003  Cajus Pollmeier
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+restore_error_handler();
+function getFAIScript ($id)
+{
+restore_error_handler();
+  $ldap = $_SESSION['config']->get_ldap_link();
+  $ldap->cd($_SESSION['config']->current['BASE']);
+
+  $sr= $ldap->cat($id);
+  $ei= ldap_first_entry($ldap->cid, $sr);
+  $tmp = ldap_get_values_len($ldap->cid, $ei,"FAIscript");
+  $tmp2 = $ldap->fetch();
+  $name= $tmp2['cn'][0];
+
+  if(isset($tmp[0])){
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+    header("Cache-Control: post-check=0, pre-check=0");
+    header("Content-type: application/octet-stream");
+    header("Content-Disposition: attachment; filename=".$name.".FAIscript");
+    echo $tmp[0];
+  }else{
+    echo sprintf("Can't query for this item '%s'",$id);
+  }
+}
+
+/* Basic setup, remove eventually registered sessions */
+@require_once ("../include/php_setup.inc");
+@require_once ("functions.inc");
+error_reporting (E_ALL);
+session_start ();
+
+/* Logged in? Simple security check */
+if (!isset($_SESSION['ui'])){
+  gosa_log ("Error: getFAIScript.php called without session");
+  header ("Location: ../index.php");
+  exit;
+}
+$ui= $_SESSION["ui"];
+$config= $_SESSION['config'];
+
+/* Check ACL's */
+$acl= get_permissions ($config->current['BASE'], $ui->subtreeACL);
+$acl= get_module_permission($acl, "all", $config->current['BASE']);
+if (chkacl($acl, "all") != ""){
+  header ("Location: ../index.php");
+  exit;
+}
+$dir = search_config($config->data,"environment", "KIOSKPATH");
+getFAIScript(base64_decode($_GET['id']));
+
+// vim:tabstop=2:expandtab:shiftwidth=2:filetype=php:syntax:ruler:
+?>
