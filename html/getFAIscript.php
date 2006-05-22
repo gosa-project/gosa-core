d4729740d1db57e957c1c6667edf37f5b09e99e1
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2006-05-22 03:39:40 +0000

    Fixed new window for script / hook downlaod
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@3470 594d385d-05f5-0310-b6e9-bd551577e9d8

--- html/getFAIscript.php
+++ html/getFAIscript.php
@@ -49,6 +49,7 @@ restore_error_handler();
     header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
     header("Cache-Control: no-cache");
     header("Pragma: no-cache");
+    header("Content-transfer-encoding: binary\n");
     header("Cache-Control: post-check=0, pre-check=0");
     header("Content-type: application/octet-stream");
     header("Content-Disposition: attachment; filename=".$name);
