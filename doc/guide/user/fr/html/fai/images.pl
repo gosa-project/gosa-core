1612880337b3b5c7ae696d4a5380b3b2d1e8067d
opensides <opensides@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-08-06 09:53:53 +0000

    - First Version of fai online manual french
    git-svn-id: https://oss.gonicus.de/repositories/gosa/branches/2.5@6967 594d385d-05f5-0310-b6e9-bd551577e9d8

---
+++ doc/guide/user/fr/html/fai/images.pl
@@ -0,0 +1,12 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/ensuremath{²};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="11" HEIGHT="18" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\ensuremath{²}">|; 
+
+1;
+
