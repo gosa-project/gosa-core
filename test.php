4da044c484ef4c02b5cd7e56f9d79837abedfb3f
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-11-06 10:24:11 +0000

    Added php page. Modified generated output
    git-svn-id: https://oss.gonicus.de/repositories/gosa/branches/2.6-playground@7738 594d385d-05f5-0310-b6e9-bd551577e9d8

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/transitional.dtd">
+<html>
+<head>
+</head>
+<body>
+<div style='overflow:scroll;height:500px; width:700px;'>
+<?php
+echo passthru("./test");
+?>
+</div>
+</body>
+</html>
