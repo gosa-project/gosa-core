c3ed5dc319429700b4cd6200b130479133d593eb
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-11-06 11:21:30 +0000

    seperated header & footer
    git-svn-id: https://oss.gonicus.de/repositories/gosa/branches/2.6-playground@7739 594d385d-05f5-0310-b6e9-bd551577e9d8

--- test.php
+++ test.php
@@ -2,9 +2,38 @@
 	"http://www.w3.org/TR/html4/transitional.dtd">
 <html>
 <head>
+<style type="text/css">
+	
+.ObjectListViewport_Headline_Table {
+	background-color: #FF0000;
+	width:100%;
+	height:20px;
+	border-collapse:collapse;
+}
+.ObjectListViewport_Headline_Cell {
+	border: 1px solid #000000;
+}
+.ObjectListViewport_Entry_Table {
+	background-color: #00FF00;
+	width:100%;
+	border-collapse:collapse;
+}
+.ObjectListViewport_Entry_Cell {
+	border: 1px solid #000000;
+}
+.ObjectListViewport_Footer_Table {
+	background-color: #0000FF;
+	height:20px;
+	width:100%;
+	border-collapse:collapse;
+}
+.ObjectListViewport_Footer_Cell {
+	border: 1px solid #000000;
+}
+</style>
 </head>
 <body>
-<div style='overflow:scroll;height:500px; width:700px;'>
+<div style='height:500px; width:700px;'>
 <?php
 echo passthru("./test");
 ?>
