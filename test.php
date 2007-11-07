9a8a8bbe62485077c56112105853d90399b67f33
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-11-07 14:51:39 +0000

    Allow multiple lists on one page
    git-svn-id: https://oss.gonicus.de/repositories/gosa/branches/2.6-playground@7747 594d385d-05f5-0310-b6e9-bd551577e9d8

--- test.php
+++ test.php
@@ -64,6 +64,9 @@
 	margin:0px;
 	padding:0px;
 }
+.ObjectListViewport_Entry_Cover{
+	overflow:auto;
+}
 .ObjectListViewport_Entry_Cell {
 	border-left: solid 1px;
 	font-size:10px;
@@ -82,36 +85,56 @@ window.onresize = updateObjectListViewportSize;
 	function updateObjectListViewportSize() {
 
 		var dbg = document.getElementById('debug');
-
+		
 		/* Somehow IE do not need to be resized 
          *  So skip the following part if the client uses ie
          */
 		if(document.all){
-			document.getElementById('ObjectListViewport_Entry_Cover').style.height= "100%";
+			var all = getElementsByStyleClass('ObjectListViewport_Entry_Cover');
+			for (var i = 0; i < all.length; ++i){
+				document.getElementById(all[i].id).style.height= "100%";
+			}
 			return;
 		}
 	
-		/* Set Viewport to min height, to ensure 
-         *  that resize will work correctly in konqueror 
-         */
-		document.getElementById('ObjectListViewport_Entry_Cover').style.height= "50px";
+		var all = getElementsByStyleClass('ObjectListViewport');
+		for (var i = 0; i < all.length; ++i){
 
-		/* Get values of displayed header and footer heights to be 
-		 *  able to recalculate the Viewport 
-         */
-		var viewport	= getObjectHeight('ObjectListViewport_Table');
-		var header  	= getObjectHeight('ObjectListViewport_TD_Header');
-		var footer  	= getObjectHeight('ObjectListViewport_TD_Footer');
+			var id_pref = all[i].id.replace(/[^0-9]*/,"");
 
-		/* Calculate the new visible entry part height. */
-		var calc = (viewport  - ( header + footer ));
-		document.getElementById('ObjectListViewport_Entry_Cover').style.height = calc;
+			/* Set Viewport to min height, to ensure 
+			 *  that resize will work correctly in konqueror 
+			 */
+			document.getElementById('ObjectListViewport_Entry_Cover' + id_pref).style.height= "50px";
 
-		/* Reduce width of entry list, we need some space to 
-         *  display the scrollbar without breaking the layout
-         */
-		var viewport = getObjectWidth('ObjectListViewport_Table');
-		document.getElementById('ObjectListViewport_Entry_Table').style.width = (viewport - 16) + 'px';
+			/* Get values of displayed header and footer heights to be 
+			 *  able to recalculate the Viewport 
+			 */
+			var viewport	= getObjectHeight('ObjectListViewport_Table' + id_pref);
+			var header  	= getObjectHeight('ObjectListViewport_TD_Header' + id_pref);
+			var footer  	= getObjectHeight('ObjectListViewport_TD_Footer' + id_pref);
+
+			/* Calculate the new visible entry part height. */
+			var calc = (viewport  - ( header + footer ));
+			document.getElementById('ObjectListViewport_Entry_Cover' + id_pref).style.height = calc;
+
+			/* Reduce width of entry list, we need some space to 
+			 *  display the scrollbar without breaking the layout
+			 */
+			var viewport = getObjectWidth('ObjectListViewport_Table' + id_pref);
+			document.getElementById('ObjectListViewport_Entry_Table' + id_pref).style.width = (viewport - 16) + 'px';
+		}
+	}
+
+	
+	/* Return all elements of a specific class */	
+	function getElementsByStyleClass (className) {
+		var all = document.all ? document.all :	document.getElementsByTagName('*');
+		var elements = new Array();
+		for (var e = 0; e < all.length; e++)
+			if (all[e].className == className)
+				elements[elements.length] = all[e];
+		return elements;
 	}
 
 
@@ -142,14 +165,13 @@ window.onresize = updateObjectListViewportSize;
 				hallo<br>
 			</td>
 			<td style='background-color:#EEEEEE;'>
-				<?php
-				echo passthru("./test");
-				?>
+				<?php echo passthru("./test"); ?>
 			</td>
 		</tr>
 		<tr>
 			<td style='background-color:#999777;'>
 				hallo<br>
+				<?php echo passthru("./test"); ?>
 				hallo<br>
 			</td>
 			<td style='background-color:#AAA006;'>
@@ -159,7 +181,7 @@ window.onresize = updateObjectListViewportSize;
 		</tr>
 	</table>
 <script type="text/javascript">
-	document.getElementById('ObjectListViewport_Entry_Cover').style.height= "50px";
+updateObjectListViewportSize();
 </script>
 </body>
 </html>
