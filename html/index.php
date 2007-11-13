356897aedf8d2dee40316857f40813072dc33ea5
cajus <cajus@594d385d-05f5-0310-b6e9-bd551577e9d8>
2007-11-13 18:41:10 +0000

    Updated to load index.php from html directory
    git-svn-id: https://oss.gonicus.de/repositories/gosa/branches/2.6-playground@7793 594d385d-05f5-0310-b6e9-bd551577e9d8

--- html/index.php
+++ html/index.php
@@ -1,6 +1,4 @@
-#!/usr/bin/php
 <?php
-
 
 /* This is good for testing... */
 error_reporting (E_ALL | E_STRICT);
@@ -14,13 +12,13 @@ function normalizePreg($input)
 
 
 /* Initiate autoloader... */
-require_once("include/autoload.inc");
+require_once("../include/autoload.inc");
 restore_error_handler();
 try {
 
 	/* Get new test instance of the Configuration */
 	$cr= Registry::getInstance("ConfigManager");
-	$cr->load("gosa.conf");
+	$cr->load("../gosa.conf");
 
 	/* Get a new instance of the main Viewport */
 	$vc= new ViewportController();
