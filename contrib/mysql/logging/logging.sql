dc5fae2c957a97c51f71286ba0d094a60d516c33
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-02-27 15:20:49 +0000

    Updated mysql logging schema
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@9178 594d385d-05f5-0310-b6e9-bd551577e9d8

--- contrib/mysql/logging/logging.sql
+++ contrib/mysql/logging/logging.sql
@@ -1,17 +1,28 @@
 create database gosa_log;
 use gosa_log;
 
+
+CREATE TABLE `gosa_locations` (
+  `id` int(11) NOT NULL auto_increment,
+  `location` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+
 CREATE TABLE `gosa_log` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `timestamp` int(10) NOT NULL,
-  `user` longtext NOT NULL,
+  `user` text NOT NULL,
   `action` varchar(255) NOT NULL,
   `objecttype` varchar(255) NOT NULL,
-  `object` varchar(255) NOT NULL,
+  `object` text NOT NULL,
   `changes` blob NOT NULL,
   `result` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-  KEY `timestamp` (`timestamp`)
+  `location_id` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
   KEY `action` (`action`),
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  KEY `timestamp` (`timestamp`),
+  KEY `objecttype` (`objecttype`),
+  KEY `result` (`result`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
