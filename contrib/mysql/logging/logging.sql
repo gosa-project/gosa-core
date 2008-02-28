922b12cbfa1f3a9c580c30cdf3c6fc6a5078b65b
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2008-02-28 09:11:21 +0000

    Upated contrib logging sql.
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@9180 594d385d-05f5-0310-b6e9-bd551577e9d8

--- contrib/mysql/logging/logging.sql
+++ contrib/mysql/logging/logging.sql
@@ -1,14 +1,15 @@
-create database gosa_log;
-use gosa_log;
 
+CREATE DATABASE `gosa_log`;
+USE `gosa_log`;
 
+DROP TABLE IF EXISTS `gosa_locations`;
 CREATE TABLE `gosa_locations` (
   `id` int(11) NOT NULL auto_increment,
   `location` text NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
-
+DROP TABLE IF EXISTS `gosa_log`;
 CREATE TABLE `gosa_log` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `timestamp` int(10) NOT NULL,
@@ -24,5 +25,17 @@ CREATE TABLE `gosa_log` (
   KEY `timestamp` (`timestamp`),
   KEY `objecttype` (`objecttype`),
   KEY `result` (`result`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+
+-- %HOST%               The name of the source host (e.g. locahost,vserver-01,%  WHERE % allows all)
+-- %USER%               The username
+-- %PWD%                The password for the new user
+
+-- CREATE USER '%USER%'@'%HOST%' IDENTIFIED BY '%PWD%';
+
+-- GRANT  SELECT , INSERT , UPDATE , DELETE
+-- ON `gosa_log`.*
+-- TO '%USER%'@'%HOST%'
+-- IDENTIFIED BY '%PWD%';
 
