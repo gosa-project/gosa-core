52a3bed1dd9bb9faead3205478e77c21e1b5ae2c
cajus <cajus@594d385d-05f5-0310-b6e9-bd551577e9d8>
2010-03-03 11:49:07 +0000

    Updated styles
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@15916 594d385d-05f5-0310-b6e9-bd551577e9d8

--- html/main.php
+++ html/main.php
@@ -268,7 +268,7 @@ if (isset($_GET['reset'])){
 $smarty->assign ("logo", "<div style='float:left'>".image("themes/modern/images/logo.png")."</div>");
 $smarty->assign ("date", date("l, dS F Y H:i:s O"));
 $smarty->assign ("lang", preg_replace('/_.*$/', '', $lang));
-$smarty->assign ("must", "<font class=\"must\">*</font>");
+$smarty->assign ("must", "<span class='required'>*</span>");
 if (isset($plug)){
   $plug= "?plug=$plug";
 } else {
