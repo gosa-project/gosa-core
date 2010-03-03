f815163b1bf8481839245a448f6697b20245a788
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2010-03-03 11:08:29 +0000

    Updated smarty image method
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@15908 594d385d-05f5-0310-b6e9-bd551577e9d8

--- include/smarty/plugins/function.image.php
+++ include/smarty/plugins/function.image.php
@@ -3,12 +3,11 @@
 function smarty_function_image($params, &$smarty)
 {
   $path = (isset($params['path']))? $params['path'] :"";
-  $label = (isset($params['label']))? $params['label'] :"";
   $action = (isset($params['action']))? $params['action'] :"";
   $title = (isset($params['title']))? $params['title'] :"";
   $align = (isset($params['align']))? $params['align'] :"";
   //print_a(array($path,$label,$action,$title,$align));
-  echo image($path,$label,$action,$title,$align);
+  echo image($path,$action,$title,$align);
 }
   
 ?>
