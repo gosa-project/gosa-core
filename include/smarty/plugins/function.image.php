94b7d3b69711bf5b44109292fbef2d7a635ec0c9
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2010-03-03 15:27:46 +0000

    Updated smarty image function
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@15996 594d385d-05f5-0310-b6e9-bd551577e9d8

--- include/smarty/plugins/function.image.php
+++ include/smarty/plugins/function.image.php
@@ -8,6 +8,13 @@ function smarty_function_image($params, &$smarty)
   $align = (isset($params['align']))? $params['align'] :"";
   //print_a(array($path,$label,$action,$title,$align));
 
+  if(isset($params['acl'])){
+    if(!preg_match("/w/", $params['acl'])){
+      $path = preg_replace("/\.png/","-grey.png", $path);
+      $action = "";
+    }
+  }
+
   if(!empty($align)){
     echo image($path,$action,$title,$align);
   }else{
