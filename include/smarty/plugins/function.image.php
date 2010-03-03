2e50c741db027c640595fc168fc8f04eca649ce0
hickert <hickert@594d385d-05f5-0310-b6e9-bd551577e9d8>
2010-03-03 11:40:56 +0000

    Updated smarty image function Fixed listing class. Single actiontrigger entries will now be rendered .
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@15911 594d385d-05f5-0310-b6e9-bd551577e9d8

--- include/smarty/plugins/function.image.php
+++ include/smarty/plugins/function.image.php
@@ -7,7 +7,12 @@ function smarty_function_image($params, &$smarty)
   $title = (isset($params['title']))? $params['title'] :"";
   $align = (isset($params['align']))? $params['align'] :"";
   //print_a(array($path,$label,$action,$title,$align));
-  echo image($path,$action,$title,$align);
+
+  if(!empty($align)){
+    echo image($path,$action,$title,$align);
+  }else{
+    echo image($path,$action,$title);
+  }
 }
   
 ?>
