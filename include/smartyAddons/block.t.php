a267c09810ee32483cafab31a5d5109c20a6d89b
cajus <cajus@594d385d-05f5-0310-b6e9-bd551577e9d8>
2012-06-19 07:01:08 +0000

    Reverted smarty workaround
    git-svn-id: https://oss.gonicus.de/repositories/gosa/trunk@21164 594d385d-05f5-0310-b6e9-bd551577e9d8

--- include/smartyAddons/block.t.php
+++ include/smartyAddons/block.t.php
@@ -73,11 +73,6 @@ function smarty_gettext_strarg($str)
 function smarty_block_t($params, $text, &$smarty)
 {
 	$text = stripslashes($text);
-
-	//TODO: need to figure out why this happens for every string...
-	if ($text == "") {
-		return ("");
-	}
 	
 	// set escape mode
 	if (isset($params['escape'])) {
@@ -124,7 +119,7 @@ function smarty_block_t($params, $text, &$smarty)
 				break;
 		}
 	}
-
+	
 	return $text;
 }
 
