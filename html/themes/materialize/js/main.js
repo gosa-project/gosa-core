07a1f2f01fcce4c2b81cdb70a73073752c9bb24a
Sebastian Sternfeld <sternfeld@gonicus.de>
2021-08-16 11:26:57 +0200

    js pw input-field null coallescing

--- html/themes/materialize/js/main.js
+++ html/themes/materialize/js/main.js
@@ -36,92 +36,94 @@ let pwStrength = [false, false, false, false, false];
 var pwButton = document.getElementById("password_finish")
 
 // When the user starts to type something inside the password field
-input.onkeyup = function () {
-    testPasswordCss(input.value);
+if (input) {
+    input.onkeyup = function () {
+        testPasswordCss(input.value);
 
-    // Validate length
-    if (input.value.length >= 8) {
-        pwLength.innerText = "check";
-        pwLength.style.color = "green";
-        pwStrength[0] = true;
-    } else {
-        pwLength.innerText = "close";
-        pwLength.style.color = "red";
-        pwStrength[0] = false;
-    }
+        // Validate length
+        if (input.value.length >= 8) {
+            pwLength.innerText = "check";
+            pwLength.style.color = "green";
+            pwStrength[0] = true;
+        } else {
+            pwLength.innerText = "close";
+            pwLength.style.color = "red";
+            pwStrength[0] = false;
+        }
 
-    // Validate lowercase letters
-    if (pwLowercase_eval) {
-        var lowerCaseLetters = /[a-z]/g;
-        if (input.value.match(lowerCaseLetters)) {
-            pwLowercase.innerText = "check";
-            pwLowercase.style.color = "green";
+        // Validate lowercase letters
+        if (pwLowercase_eval) {
+            var lowerCaseLetters = /[a-z]/g;
+            if (input.value.match(lowerCaseLetters)) {
+                pwLowercase.innerText = "check";
+                pwLowercase.style.color = "green";
+                pwStrength[1] = true;
+            } else {
+                pwLowercase.innerText = "close";
+                pwLowercase.style.color = "red";
+                pwStrength[1] = false;
+            }
+        } else {
             pwStrength[1] = true;
-        } else {
-            pwLowercase.innerText = "close";
-            pwLowercase.style.color = "red";
-            pwStrength[1] = false;
         }
-    } else {
-        pwStrength[1] = true;
-    }
 
 
-    // Validate capital letters
-    if (pwUppercase_eval) {
-        var upperCaseLetters = /[A-Z]/g;
-        if (input.value.match(upperCaseLetters)) {
-            pwUppercase.innerText = "check";
-            pwUppercase.style.color = "green";
+        // Validate capital letters
+        if (pwUppercase_eval) {
+            var upperCaseLetters = /[A-Z]/g;
+            if (input.value.match(upperCaseLetters)) {
+                pwUppercase.innerText = "check";
+                pwUppercase.style.color = "green";
+                pwStrength[2] = true;
+            } else {
+                pwUppercase.innerText = "close";
+                pwUppercase.style.color = "red";
+                pwStrength[2] = false;
+            }
+        } else {
             pwStrength[2] = true;
-        } else {
-            pwUppercase.innerText = "close";
-            pwUppercase.style.color = "red";
-            pwStrength[2] = false;
         }
-    } else {
-        pwStrength[2] = true;
-    }
 
-    // Validate numbers
-    if (pwNumber_eval) {
-        var numbers = /[0-9]/g;
-        if (input.value.match(numbers)) {
-            pwNumbers.innerText = "check";
-            pwNumbers.style.color = "green";
+        // Validate numbers
+        if (pwNumber_eval) {
+            var numbers = /[0-9]/g;
+            if (input.value.match(numbers)) {
+                pwNumbers.innerText = "check";
+                pwNumbers.style.color = "green";
+                pwStrength[3] = true;
+            } else {
+                pwNumbers.innerText = "close";
+                pwNumbers.style.color = "red";
+                pwStrength[3] = false;
+            }
+        } else {
             pwStrength[3] = true;
-        } else {
-            pwNumbers.innerText = "close";
-            pwNumbers.style.color = "red";
-            pwStrength[3] = false;
+
         }
-    } else {
-        pwStrength[3] = true;
-
-    }
 
 
-    // Validate numbers
-    if (pwSpecial_eval) {
-        var specialChars = /[!,@,#,$,%,^,&,*,?,_,~]/g;
-        if (input.value.match(specialChars)) {
-            pwSpecial.innerText = "check";
-            pwSpecial.style.color = "green";
+        // Validate numbers
+        if (pwSpecial_eval) {
+            var specialChars = /[!,@,#,$,%,^,&,*,?,_,~]/g;
+            if (input.value.match(specialChars)) {
+                pwSpecial.innerText = "check";
+                pwSpecial.style.color = "green";
+                pwStrength[4] = true;
+            } else {
+                pwSpecial.innerText = "close";
+                pwSpecial.style.color = "red";
+                pwStrength[4] = false;
+            }
+        } else {
             pwStrength[4] = true;
-        } else {
-            pwSpecial.innerText = "close";
-            pwSpecial.style.color = "red";
-            pwStrength[4] = false;
+
         }
-    } else {
-        pwStrength[4] = true;
-
-    }
 
 
-    if (pwStrength.every(Boolean)) {
-        pwButton.disabled = false;
-    } else {
-        pwButton.disabled = true;
+        if (pwStrength.every(Boolean)) {
+            pwButton.disabled = false;
+        } else {
+            pwButton.disabled = true;
+        }
     }
 }
