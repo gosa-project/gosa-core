c2c35cd0080019ced9522f29eca927755366643a
Lara Suttmann <suttmann@gonicus.de>
2021-10-26 14:40:00 +0200

    Automatic width adjustment of content navigation during resize. Debounce function included

--- html/themes/default/js/eventListener.js
+++ html/themes/default/js/eventListener.js
@@ -59,24 +59,8 @@ let navInstances = M.Dropdown.init(nav, {
     'alignment': 'right',
 });
 
-let tabletareaSelection = document.querySelectorAll('.dropdown-tablet-area-selection');
-let tabletareaInstances = M.Dropdown.init(tabletareaSelection, {
-    'constrainWidth': false,
-    'coverTrigger': false,
-    'hover': false,
-    'alignment': 'right'
-});
-
-let mobileareaSelection = document.querySelectorAll('.dropdown-mobile-area-selection');
-let mobileareaInstances = M.Dropdown.init(mobileareaSelection, {
-    'constrainWidth': false,
-    'coverTrigger': false,
-    'hover': false,
-    'alignment': 'right'
-});
-
-let areaSelection = document.querySelectorAll('.dropdown-area-selection');
-let areaInstances = M.Dropdown.init(areaSelection, {
+let advancedareaSelection = document.querySelectorAll('.dropdown-advanced-area-selection');
+let advancedareaInstances = M.Dropdown.init(advancedareaSelection, {
     'constrainWidth': false,
     'coverTrigger': false,
     'hover': false,
