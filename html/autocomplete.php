11f05114f5eb0fa6ef17eded46fe2f0863621881
Benjamin Zapiec <zapiec@gonicus.de>
2025-03-26 17:51:27 +0100

    update code sytle

--- html/autocomplete.php
+++ html/autocomplete.php
@@ -21,31 +21,31 @@
  */
 
 /* Basic setup, remove eventually registered sessions */
-require_once ("../include/php_setup.inc");
+require_once("../include/php_setup.inc");
 
 session_cache_limiter("private");
 session::start();
-session::global_set('errorsAlreadyPosted',array());
+session::global_set('errorsAlreadyPosted', array());
 
 /* Logged in? Simple security check */
-if (!session::global_is_set('ui')){
-    new log("security","unknown","",array(),"Error: autocomplete.php called without session") ;
-    header ("Location: index.php");
+if (!session::global_is_set('ui')) {
+    new log("security", "unknown", "", array(), "Error: autocomplete.php called without session");
+    header("Location: index.php");
     exit;
 }
 
 /* Base completition or filter completition? */
 if (isset($_GET['type']) && $_GET['type'] == "base" && isset($_GET['pid'])) {
-    
+
     // Find dn based on name and description
     $pid = $_GET['pid'];
-    if (session::is_set("pathMapping_{$pid}")){
-        $res= "";
-        $pathMapping= session::get("pathMapping_{$pid}");
-        $department_info= session::get("department_info_{$pid}");
+    if (session::is_set("pathMapping_{$pid}")) {
+        $res = "";
+        $pathMapping = session::get("pathMapping_{$pid}");
+        $department_info = session::get("department_info_{$pid}");
 
 
-        $search= preg_replace('/&quot;/', '"', current($_POST));
+        $search = preg_replace('/&quot;/', '"', current($_POST));
         foreach ($department_info as $dn => $info) {
 
             if (!isset($pathMapping[$dn])) {
@@ -54,18 +54,18 @@ if (isset($_GET['type']) && $_GET['type'] == "base" && isset($_GET['pid'])) {
                 continue;
             }
             if (mb_stristr($info['name'], $search) !== false) {
-                $res.= "<li>".mark($search, $pathMapping[$dn]).($info['description']==''?""
-                        :"<span class='informal'> [".mark($search, $info['description'])."]</span>")."</li>";
+                $res .= "<li>" . mark($search, $pathMapping[$dn]) . ($info['description'] == '' ? ""
+                    : "<span class='informal'> [" . mark($search, $info['description']) . "]</span>") . "</li>";
                 continue;
             }
             if (mb_stristr($info['description'], $search) !== false) {
-                $res.= "<li>".mark($search, $pathMapping[$dn]).($info['description']==''?""
-                        :"<span class='informal'> [".mark($search, $info['description'])."]</span>")."</li>";
+                $res .= "<li>" . mark($search, $pathMapping[$dn]) . ($info['description'] == '' ? ""
+                    : "<span class='informal'> [" . mark($search, $info['description']) . "]</span>") . "</li>";
                 continue;
             }
             if (mb_stristr($pathMapping[$dn], $search) !== false) {
-                $res.= "<li>".mark($search, $pathMapping[$dn]).($info['description']==''?""
-                        :"<span class='informal'> [".mark($search, $info['description'])."]</span>")."</li>";
+                $res .= "<li>" . mark($search, $pathMapping[$dn]) . ($info['description'] == '' ? ""
+                    : "<span class='informal'> [" . mark($search, $info['description']) . "]</span>") . "</li>";
                 continue;
             }
         }
@@ -75,18 +75,15 @@ if (isset($_GET['type']) && $_GET['type'] == "base" && isset($_GET['pid'])) {
             echo "<ul>$res</ul>";
         }
     }
-
 } else {
 
     $ui = session::global_get('ui');
     $config = session::global_get('config');
 
     /* Is there a filter object arround? */
-    if (session::is_set("autocomplete")){
-		$name= session::get("autocomplete");
-		$filter = session::get($name);
+    if (session::is_set("autocomplete")) {
+        $name = session::get("autocomplete");
+        $filter = session::get($name);
         $filter->processAutocomplete();
     }
 }
-
-?>
